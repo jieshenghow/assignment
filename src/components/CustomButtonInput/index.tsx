75568bf3bda4de55bc18301a49c35cfb18a0ad97
jieshenghow <octol3er.dev@gmail.com>
2022-11-11 15:46:49 +0800

    first commit

--- src/components/CustomButtonInput/index.tsx
+++ src/components/CustomButtonInput/index.tsx
@@ -1,21 +1,21 @@
 import {Props} from "./props";
-import {Button, VStack, Text, HStack, Box, Image} from "@chakra-ui/react";
-import React, {useEffect, useState} from "react";
+import {HStack, Image, Text, VStack} from "@chakra-ui/react";
+import React, {useState} from "react";
 import ImageUploading from "react-images-uploading"
 
 
+export const CustomButtonInput = (props: Props) => {
+  const {label, selection, isRequired = false, prefix, imageCallback} = props
 
-export const CustomButtonInput = (props: Props ) => {
-  const {label, isMulti = false, selection, isRequired=false , prefix, imageCallback} = props
+  const [selected, setSelected] = useState<{ label: string, value: string }>()
 
-  const [selected, setSelected] = useState<{label: string, value:string}>()
-
-  const handleSelect = (value: string, label:string) => {
+  const handleSelect = (value: string, label: string) => {
     setSelected({
       label,
       value
     })
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [images, setImages] = React.useState([]);
 
   const onChange = (imageList: any, addUpdateIndex: any) => {
@@ -23,8 +23,7 @@ export const CustomButtonInput = (props: Props ) => {
   }
 
 
-
-  return(
+  return (
     <VStack w={"100%"} align={"start"}>
       <HStack>
         <Text fontWeight={700}>{label}</Text>
@@ -34,18 +33,19 @@ export const CustomButtonInput = (props: Props ) => {
         {selection.map((item, index) => {
           const backgroundColor = selected?.value === item.value ? "#000000" : "#FFFFFF"
           const textColor = selected?.value === item.value ? "#FFFFFF" : "#000000"
-          return(
+          return (
             <ImageUploading multiple={true} key={index} onChange={onChange} value={images}>
-              {({ imageList, onImageUpload, onImageRemoveAll }) => (
-                <HStack onClick={()=>{
-                  if (prefix){
+              {({imageList, onImageUpload, onImageRemoveAll}) => (
+                <HStack onClick={() => {
+                  if (prefix) {
                     onImageUpload()
-                  }else{
+                  } else {
                     handleSelect(item.value, item.label)
 
                   }
-                }} backgroundColor={backgroundColor} color={textColor}  borderWidth={1} borderColor={"#202020"} padding={2} justifyContent={"center"}>
-                  {prefix && <Image w={15} h={15} src={prefix} />}
+                }} backgroundColor={backgroundColor} color={textColor} borderWidth={1} borderColor={"#202020"}
+                        padding={2} justifyContent={"center"}>
+                  {prefix && <Image w={15} h={15} src={prefix}/>}
                   <Text>{item.label}</Text>
                 </HStack>
               )}
